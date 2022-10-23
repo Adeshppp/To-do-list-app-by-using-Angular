05296d2d81b8170004db419758f747efdfcddcf2
Adesh <adeshp948@gmail.com>
2022-10-23 09:27:00 -0400

    Added a feature: Added data(to-do list made by user) of the user to the local storage. so if the user refreshes the page even after making a list, the whole to-do list will show up.

--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -7,12 +7,19 @@ import {Todo } from "../../Todo";
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
-
+  localItem : string | null;
   todos:Todo[];
   constructor() { 
-    this.todos = [
-      
-    ]
+    // this.currentUser = JSON.parse(localStorage.getItem('currentUser') || '{}'); og
+    // this.localItem = JSON.parse(localStorage.getItem('todos') || '{}');
+    this.localItem = localStorage.getItem("todos");
+    if(this.localItem == null){
+      this.todos=[];
+    }
+    else{
+      this.todos = JSON.parse(this.localItem);
+    } 
+
   }
 
   ngOnInit(): void {
@@ -22,10 +29,13 @@ export class TodosComponent implements OnInit {
     console.log(todo);
     const index=this.todos.indexOf(todo);
     this.todos.splice(index,1);
+    localStorage.setItem("todos",JSON.stringify(this.todos));
   }
   addTodo(todo:Todo){
     console.log(todo);
     this.todos.push(todo);
+    localStorage.setItem("todos",JSON.stringify(this.todos));
+
   }
 
 }
